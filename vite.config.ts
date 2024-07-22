70e83fac9ec1c97e5115df4bb78a21090fa45fd1
nikitakurilo <map.reduce90@gmail.com>
2024-07-22 23:22:56 +0200

    feat: 👋🏻🗺️

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import type { UserConfigExport, ConfigEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
@@ -17,23 +17,24 @@ const baseConfig: UserConfigExport = {
 }
 
 export default (configEnv: ConfigEnv) => {
+  // example. can be removed
   setTimeout(() => {
-    console.groupCollapsed('Environment Variables')
+    const envVariables = {...process.env, ...loadEnv(configEnv.mode, process.cwd())};
 
-    console.log(configEnv, 'configEnv')
+    const filteredData = Object.entries(envVariables)
+      .filter(([key]) => /(NODE|VITE)/.test(key))
+      .map(([key, value]) => {
+        return {
+          key: key.trim(),
+          value: value?.trim().slice(0, 50),
+        };
+      });
 
-    // console.table(
-    //   Object.entries(getEnv(configEnv.mode)).map(([key, value]) => {
-    //     return {
-    //       'ENV VARIABLE': key,
-    //       'Value': value,
-    //       '🌎': '👋🏻'
-    //     };
-    //   })
-    // );
-
-    console.groupEnd()
-  }, 0)
+    console.log("👋🏻🌎.These are env variables (which contains VITE and NODE prefixes)");
+    filteredData.forEach(item => {
+      console.log(`    key: ${item.key}, value: ${item.value}`);
+    });
+  });
 
   return defineConfig({ ...baseConfig })
 }
