7cea3bcea23f49e494e522dd7b64488c4e9c8783
nikitakurilo <nikitakurilo@gmail.com>
2024-07-28 23:21:16 +0200

    docs: clean up

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,17 +1,7 @@
 import BaseService from './base-service.ts'
 
-// const getMockUser = (username : string = 'reduce-map', password: string = 'hello-world') => {
-//   return {
-//     token: `your-${password}-token`,
-//     role: 'admin',
-//     username: username
-//   }
-// }
-
 export default {
   async login(username: string, password: string) {
-    // return getMockUser(username, password);
-
     // assuming next would probably do smth like that
     const { data } = await BaseService.post('/login', { username, password })
     return data
